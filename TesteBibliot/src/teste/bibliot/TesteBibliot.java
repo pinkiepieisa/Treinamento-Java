5924d02777239f0ba609765b20116e153e179f3e
Bela <imcdsls10@gmail.com>
2025-02-28 00:43:49 -0300

    Teste

---
+++ TesteBibliot/src/teste/bibliot/TesteBibliot.java
@@ -0,0 +1,11 @@
+package teste.bibliot;
+
+import javax.swing.JOptionPane;
+
+public class TesteBibliot {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Alerta!!!", "Alerta",JOptionPane.ERROR_MESSAGE);
+	}
+
+}
